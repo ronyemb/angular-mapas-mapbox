4537923c5d4f31aee75f2d6f9a5ed9aa7aac76f4
Rony Miranda <ronyemb@gmail.com>
2024-12-13 18:32:17 -0500

    Primer commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+
+export const environment = {
+  // mapbox_key: "pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2puMHhrODI1NG95MnJwOTZldjAw2xuIn0.qSVIwYh9D-wVyzUne3euog",
+  // mapbox_key: "pk.eyJ1IjoiZnJlZHlqYXZpZXIiLCJhIjoiY2xiMWExcnFyMTMyMTN3cnp0MGVjazViNiJ9.QAB8TQNWnwX6KKnT4sLpag",
+  // mapbox_key: "pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja3hramV2OWIwbjEwMzFwYzJlZWl6N2g5In0.iKXPpYvo7UPRiiZ-x_lCrw",
+  mapbox_key: "pk.eyJ1Ijoia2xlcml0aCIsImEiOiJja2puMHhrODI1NG95MnJwOTZldjAw2xuIn0.qSVIwYh9D-wVyzUne3euog",
+
+  otra: "PROPIEDAD",
+};
